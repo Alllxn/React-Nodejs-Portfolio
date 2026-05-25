6b09e8ee08c841097258b087f8812ccdb4abace6
Alllxn <allan.esib@gmail.com>
2026-05-25 15:48:34 +0100

    fix: apply green theme to contact form and fix webkit autofill styling

--- client/src/PortfolioContainer/Contact/Contact.js
+++ client/src/PortfolioContainer/Contact/Contact.js
@@ -170,10 +170,11 @@ export function Contact() {
                                 <FontAwesomeIcon icon="fa-solid fa-user-astronaut" size='xl' />
                             </span>
                             <input
+                                spellCheck="false"
                                 placeholder={f.name}
                                 name="name"
                                 type="text"
-                                className='card load-animation-element'
+                                className='card '
                                 onChange={(e) => insertData(e)}
                             />
                             {formData.name.error.exist && (
@@ -188,10 +189,11 @@ export function Contact() {
                                 <FontAwesomeIcon icon="fa-solid fa-envelope" size='xl' />
                             </span>
                             <input
+                                spellCheck="false"
                                 placeholder={f.email}
                                 name="email"
                                 type="text"
-                                className="card load-animation-element"
+                                className="card"
                                 maxLength="320"
                                 onChange={(e) => insertData(e)}
                             />
@@ -207,10 +209,11 @@ export function Contact() {
                                 <FontAwesomeIcon icon="fa-solid fa-pen" size='xl'/>
                             </span>
                             <input
+                                spellCheck="false"
                                 placeholder={f.subject}
                                 name="subject"
                                 type="text"
-                                className="card load-animation-element"
+                                className="card"
                                 maxLength="70"
                                 onChange={(e) => insertData(e)}
                             />
@@ -228,9 +231,10 @@ export function Contact() {
                                 <FontAwesomeIcon icon="fa-solid fa-message" size='xl' />
                             </span>
                             <textarea
+                                spellCheck="false"
                                 name="message"
                                 placeholder={f.message}
-                                className="card load-animation-element"
+                                className="card"
                                 onChange={(e) => insertData(e)}
                             />
                             {formData.message.error.exist && (
