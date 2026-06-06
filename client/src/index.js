38095d27941520264a981fee9618afb2ebf4518d
Alllxn <allan.esib@gmail.com>
2026-06-06 02:18:23 +0100

    feat: add Google Analytics 4 tracking

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga4';
 import './index.css';
 import App from './App';
+
+ReactGA.initialize('G-HGG7TNSY9C');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
+
+// TODO: preguntar "analiza el proyecto y dime si necesito tener policita de cookies y de proteccion de datos, además analiza que mas hace falta a la web"
+// TODO: policita de proteccion de datos
+// TODO: politica de cookies
