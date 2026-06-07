301d6d900c467102df86bb04b9db55a377c63bd6
Alllxn <allan.esib@gmail.com>
2026-06-07 01:39:03 +0100

    fix: restrict CORS to production origin

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const { Resend } = require("resend");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN || "http://localhost:3000" }));
 app.use(express.json());
 app.use("/", router);
 app.listen(PORT, () => console.log("Server Running | " + PORT));
